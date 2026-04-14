1e073a38b14d9ceeb6904122bf9868a2aaadb556
hirwa63 <hirwaolivier63@gmail.com>
2026-04-14 23:37:06 +0200

    Fix: Resolve all merge conflicts - apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,10 +2,5 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-<<<<<<< HEAD
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "accounts"
-=======
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
->>>>>>> 81d5325699351fc300714857a50f2e052ebfbb55
